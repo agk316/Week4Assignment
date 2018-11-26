2dc84e1d00044ce436cf407d7996ddb7e46386fd
agk316 <42292662+agk316@users.noreply.github.com>
2018-11-26 17:35:49 +0400

    Add files via upload

---
+++ CodeBook.md
@@ -0,0 +1,22 @@
+CodeBook.MD
+
+Variables
+featureLabels - Stores the Column names of the 561 measurements taken
+VariableData - Stores the actual Measurements x subjects x activity
+activity - Stores the single column of activity data
+activityLabels - Contains the numeric and string data of the Activity to be matched later
+activity2 - Combines the Label with the activity data column
+Testdata / Train Data - Putting the data together from all the previous inputs
+Testdata2 / Train Data2 - Melts the measurements from being individual columns into one column to be easier to manipulate later on
+Testdata3 / Train Data3 - filters the previous table to only keep STD and MEAN entries and ignore all else
+MergeData - The Merged data including Test and Train cases
+AggData - The aggregated Mean data of the previous table. To only have one response per measurement per subject x activity
+
+Transformations
+1. Load the actual measurements data and assign columns from the featureLabels file
+2. Match the Activity numerals with the characters
+3. Put it all together with the "TEST/TRAIN" information if it may be required later on
+4. Melt the measurements from individual columns into 1 column
+5. Keep only STD and Mean entries and remove other rows
+6. Merge Test and Train Data
+7. Aggregate the scores to have only one entry for each measurement
---
+++ run_analysis.R
@@ -0,0 +1,65 @@
+library(dplyr)
+library(plyr)
+library(reshape2)
+
+setwd("C://Users//arun.gopalkrishnan//Documents//R Work//proj")
+
+featureLabels <- read.table("features.txt")
+featureLabels$V3 <- gsub("\\.","",make.names(featureLabels$V2))
+
+#--Put Test Data in Shape
+
+VariableData <- read.table("test/X_test.txt", col.names = featureLabels$V3)
+activity <- read.table("test/Y_test.txt")
+
+activityLabels <- read.table("activity_labels.txt")
+activity2 <- join(activity,activityLabels,by = "V1", type="left")
+
+Testdata <- data.frame(read.table("test/subject_test.txt"), "TEST", activity2$V2,VariableData)
+names(Testdata)[1] <- "SUB_ID"
+names(Testdata)[2] <- "TYPE"
+names(Testdata)[3] <- "ACTIVITY"
+
+Testdata2 <- melt(Testdata, id=c("SUB_ID","TYPE","ACTIVITY"),measure.vars = c(4:564))
+
+Testdata3 <- subset(Testdata2, grepl("*mean*",Testdata2$variable, ignore.case = TRUE) | grepl("*std*",Testdata2$variable, ignore.case = TRUE))
+
+
+
+#--Put Train Data in Shape
+
+VariableData <- read.table("train/X_train.txt", col.names = featureLabels$V3)
+
+activity <- read.table("train/Y_train.txt")
+
+activityLabels <- read.table("activity_labels.txt")
+activity2 <- join(activity,activityLabels,by = "V1", type="left")
+
+Traindata <- data.frame(read.table("train/subject_train.txt"), "TRAIN", activity2$V2,VariableData)
+names(Traindata)[1] <- "SUB_ID"
+names(Traindata)[2] <- "TYPE"
+names(Traindata)[3] <- "ACTIVITY"
+
+Traindata2 <- melt(Traindata, id=c("SUB_ID","TYPE","ACTIVITY"),measure.vars = c(4:564))
+
+Traindata3 <- subset(Traindata2, grepl("*mean*",Traindata2$variable, ignore.case = TRUE) | grepl("*std*",Traindata2$variable, ignore.case = TRUE))
+
+
+
+#--Merge Test and Train - Q1
+
+MergeData <- rbind(Testdata3, Traindata3)
+head(MergeData,5)
+
+
+# ---- Aggregate of each variable - Q2
+
+AggData <- aggregate(MergeData$value, by=list(MergeData$SUB_ID,MergeData$TYPE,MergeData$ACTIVITY,MergeData$variable),FUN=mean)
+names(AggData)
+names(AggData)[1] <- "SUB_ID"
+names(AggData)[2] <- "TYPE"
+names(AggData)[3] <- "ACTIVITY"
+names(AggData)[4] <- "Variable"
+names(AggData)[5] <- "Value"
+
+head(AggData)
